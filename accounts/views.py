88fd0700d710e368e1f253e72a97bf896b4b0516
tassnimelleuch <tasnim.eleuch@enis.tn>
2026-04-15 17:08:47 +0100

    testing jenkins users

--- accounts/views.py
+++ accounts/views.py
@@ -202,8 +202,3 @@ def delete_phone(request, phone_id):
         return redirect('contact_detail', contact.id)
 
     return render(request, 'accounts/delete_phone.html', {'phone': phone, 'contact': contact})
-
-
-def testing_security():
-
-    password = "this is a function to test sonar fails"

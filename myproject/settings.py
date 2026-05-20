9db22d07ce3d513c940a296a48feaf48d54aea6d
tassnimelleuch <tasnim.eleuch@enis.tn>
2026-05-20 10:00:44 +0100

    testing github kpis

--- myproject/settings.py
+++ myproject/settings.py
@@ -84,7 +84,6 @@ DATABASES = {
 }
 
 
-# Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 # Minimal password validation - only check password matching
